42b50ee73b717eb0f7d79e0f8e3ba658a90c89eb
RaviRTirkey <ranjantirkeyravi@gmail.com>
2026-02-13 18:20:33 +0530

    SecurityConfig update

--- src/main/java/com/tirkey/eshop/config/SecurityConfig.java
+++ src/main/java/com/tirkey/eshop/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for stateless APIs
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll() // White-list auth endpoints
                         .requestMatchers("/api/v1/products/**").permitAll() // Allow guest browsing
