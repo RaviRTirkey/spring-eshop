7d959a2590d127c0838697ac5c0a215197e88861
RaviRTirkey <ranjantirkeyravi@gmail.com>
2026-02-13 18:24:17 +0530

    SecurityConfig update

--- src/main/java/com/tirkey/eshop/config/SecurityConfig.java
+++ src/main/java/com/tirkey/eshop/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.tirkey.eshop.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,11 +24,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for stateless APIs
-                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll() // White-list auth endpoints
-                        .requestMatchers("/api/v1/products/**").permitAll() // Allow guest browsing
-                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN") // Protect admin routes
+                        .requestMatchers(HttpMethod.OPTIONS,"/api/v1/auth/**").permitAll() // White-list auth endpoints
+                        .requestMatchers(HttpMethod.OPTIONS,"/api/v1/products/**").permitAll() // Allow guest browsing
+                        .requestMatchers(HttpMethod.OPTIONS,"/api/v1/admin/**").hasRole("ADMIN") // Protect admin routes
                         .anyRequest().authenticated() // All other requests need a token
                 )
                 .sessionManagement(session -> session
